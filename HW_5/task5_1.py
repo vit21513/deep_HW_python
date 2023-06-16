fbc1486ace0867cfaeee91eb1cad3981e1255846
vit21513 <vit21513@yandex.ru>
2023-06-16 11:02:27 +0300

    fin5

--- HW_5/task5_1.py
+++ HW_5/task5_1.py
@@ -1,17 +1,14 @@
 # напишите функцию, которая принимает на вход строку - абсолютный путь до файла.
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 
-from unittest.mock import patch
 
-
-example = 'C:/teach/алгоритм/home_work_python/home_work_python/HW_leson1/task1.py'
+example = "C:/teach/алгоритм/home_work_python/home_work_python/HW_leson1/task1.py"
 
 
 def patch_split(input_str):
-   
-    *patch, files_name,type_files =  example.replace('/','.').split('.')
-    patch = "/".join(patch)
-    return patch, files_name, type_files
+    *patch_example, files_name, type_files = example.replace("/", ".").split(".")
+    patch_example = "/".join(patch_example)
+    return patch_example, files_name, type_files
 
 
 print(patch_split(example))
