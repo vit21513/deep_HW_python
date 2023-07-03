dda5827ee5d6ee1490992ff7481598785c3fa3d6
vit21513 <vit21513@yandex.ru>
2023-07-03 18:47:46 +0300

    h9..ver2

--- HW_9/task9_1.py
+++ HW_9/task9_1.py
@@ -3,15 +3,17 @@
 # Декоратор, запускающий функцию нахождения корней квадратного уравнения с каждой тройкой чисел из csv файла.
 # Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл.
 import json
+import csv
 import os
 import random
 from math import sqrt
 
 
 def gen_csv(num=3, start_num=1, end_num=100, quantity_min=100, quantity_max=1000):
-    with open("randomlist.json", "w", encoding="utf-8") as js:
-        json.dump([[random.randint(start_num, end_num) for _ in range(num)] for _ in
-                   range(random.randint(quantity_min, quantity_max))], js)
+    with open("randomlist.csv", "w", encoding="utf-8", newline="") as js:
+        writer = csv.writer(js, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows([[random.randint(start_num, end_num) for _ in range(num)] for _ in
+                          range(random.randint(quantity_min, quantity_max))])
 
 
 def save_vareables(func):
@@ -25,18 +27,24 @@ def save_vareables(func):
         with open(func.__name__ + ".json", "w", encoding='utf-8') as result_f:
             json.dump(res, result_f, indent=1)
         return func
+
     return wrapper
 
 
 def csv_variable(func):
-    """ function reads json file and passes the values of variables to the function"""
-    if os.path.exists("randomlist.json"):
-        with open("randomlist.json", encoding='utf-8') as res_f:
-            for item in json.load(res_f):
-                func(*item)
+    def wrapper(*args, **qargs):
+
+        """ function reads json file and passes the values of variables to the function"""
+        if os.path.exists("randomlist.csv"):
+            with open("randomlist.csv", encoding='utf-8', newline="") as res_f:
+                for item in csv.reader(res_f, quoting=csv.QUOTE_NONNUMERIC):
+                    func(*item)
+
+                return func
+        else:
             return func
-    else:
-        return func
+
+    return wrapper
 
 
 @csv_variable
@@ -58,5 +66,3 @@ def check(a, b, c):
 
 # gen_csv()
 check(5, 0, -5)
-check(45,7,90)
-check(5,7,9)
